c9b06d99469ea214812f235df58c5a517c8e9841
zackblessed89-jpg <zackblessed89@gmail.com>
2025-12-11 23:14:02 -0800

    favicon

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+    amazon clone
+      </div>
+      
+    </>
+  )
+}
+
+export default App
