f296ae16b013d934bba9391436b3c1977a7a669c
zackblessed89-jpg <zackblessed89@gmail.com>
2025-12-24 00:17:57 -0800

    Header sticky

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
   const [{basket}]= useContext(DataContext)
   // console.log(basket.length)
   return (
-    <>
+    <section className={classes.fixed}>
       <section>
         <div className={classes.header_container}>
           {/* logo */}
@@ -74,7 +74,7 @@ const Header = () => {
         </div>
       </section>
       <LowerHeader />
-    </>
+    </section>
   );
 }
 
