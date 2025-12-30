fe5614479934c0d4112a239c71db77b3d2bbc205
zackblessed89-jpg <zackblessed89@gmail.com>
2025-12-29 21:54:35 -0800

    building auth page UI

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "../Auth/Auth.module.css";
 import { Link } from "react-router-dom";
+import { auth } from "../../Utility/firebase";
+import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from "firebase/auth"
 
 function Auth() {
+
+const [email,setEmail]= useState("");
+const [password,setPassword]= useState("");
+// const [error, setError]= useState("");
+// console.log(password,email);
+
+
+const authHandler = async(e)=> { e.preventDefault()
+  console.log(e.target.name);
+if (e.target.name == "signin"){
+//  firebase auth
+signInWithEmailAndPassword (auth,email,password).then((userInfo)=> {
+  console.log(userInfo);
+}).catch((err)=>{
+  console.log(err);
+});
+
+} else {
+
+  createUserWithEmailAndPassword (auth,email,password)
+  .then((userInfo)=> {
+    console.log(userInfo);
+  }).catch((err)=>{
+    console.log(err);
+  });
+
+}
+};
+
+
+
+
   return (
     <section className={classes.login}>
       {/* logo */}
@@ -19,21 +53,40 @@ function Auth() {
         <form action="">
           <div>
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              id="email"
+            />
           </div>
 
           <div>
             <label htmlFor="password"> Password</label>
-            <input type="password" id="Password" />
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              id="Password"
+            />
           </div>
-          <button className= {classes.login__signInButton}> Sign In</button>
+          <button type="submit" 
+          name="signin"
+          onClick={authHandler} className={classes.login__signInButton}> Sign In</button>
         </form>
         <p>
           {/* agreement  */}
-          By signing-in you agree to the AMAZON FAKE CLONE conditions of use & sale. Please see our Privacy Notice , our cookies Notice and our Interest -Based Ads Notice.  
+          By signing-in you agree to the AMAZON FAKE CLONE conditions of use &
+          sale. Please see our Privacy Notice , our cookies Notice and our
+          Interest -Based Ads Notice.
         </p>
         {/* create account button  */}
-        <button className= {classes.login__registerButton}> Create your Amazon Account</button>
+        <button type="submit" 
+        name="signup"
+        onClick= {authHandler}className={classes.login__registerButton}>
+          {" "}
+          Create your Amazon Account
+        </button>
       </div>
     </section>
   );
--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -20,3 +20,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app); // if you use Firebase Storage
+ export default app;
